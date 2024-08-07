1beba29da11e56412501c57fea0b48e2fd35c70d
Guilherme Zapater <guizapa368@gmail.com>
2024-08-07 14:02:08 -0300

    "gitactions and secret"

--- js/form.js
+++ js/form.js
@@ -1,31 +1,8 @@
-
-// (function(){
-//     emailjs.init("PQR1g2_86f4DTSylZ");  // Substitua por sua chave pública
-// })();
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const form = document.querySelector(".contact-form");
-
-//     form.addEventListener("submit", function(event) {
-//         event.preventDefault();
-
-//         emailjs.sendForm('service_pr707ep', 'template_0y4ysk9', form)
-//             .then(function(response) {
-//                 console.log('SUCCESS!', response.status, response.text);
-//                 alert('Mensagem enviada com sucesso!');
-//             }, function(error) {
-//                 console.log('FAILED...', error);
-//                 alert('Ocorreu um erro ao enviar a mensagem.');
-//             });
-//     });
-// });
-
-
 // Carregar a biblioteca cliente do Google
 gapi.load('client:auth2', function() {
     gapi.client.init({
-        apiKey: 'AIzaSyB_fAow_xQWvFGi3UMwgj4PMJrW32o6PTI',
-        clientId: '265372675207-aldhj3tdu0v8j083djcnfat1j2moddv4.apps.googleusercontent.com',
+        apiKey: process.env.GMAIL_API_KEY,
+        clientId: process.env.GMAIL_CLIENT_ID,
         scope: 'https://www.googleapis.com/auth/gmail.send'
     }).then(function () {
         gapi.auth2.getAuthInstance().signIn().then(function () {
