c8d81cf52d28c1753fd056b79a445a4b7ffc6c80
Guilherme Zapater <guizapa368@gmail.com>
2024-09-17 16:54:33 -0300

    menu hamburguer e isolamento da nav

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    const menu = document.getElementById('navbar-list');
+    menu.classList.toggle('open'); // Toggle da classe 'open' para abrir/fechar o menu
+});
